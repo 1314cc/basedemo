583ee307e799611d34fa09f3b2a327b82352960f
chen chen <blackchen277@gmail.com>
2019-01-08 19:47:30 +0800

    init

---
+++ demo-gateway/src/main/java/com/black/demo/Application.java
@@ -0,0 +1,25 @@
+package com.black.demo;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.black.demo.mapper")
+public class Application implements CommandLineRunner {
+    private Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("服务启动完成!");
+    }
+}
